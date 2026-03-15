80e58fd5c81899c9c4b62913c360d5391e56c2db
Bored Piper <boredpiper@gmail.com>
2026-03-15 22:45:54 +0530

    Fix kpi-builder.jsx import path to use relative path

--- src/pages/kpi-builder.jsx
+++ src/pages/kpi-builder.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { KPI_TEMPLATES, calculateKPI } from '@/lib/kpiFeatures';
+import { KPI_TEMPLATES, calculateKPI } from ..@/lib/kpiFeatures';
 import Link from 'next/link';
 
 export default function KPIBuilder() {
