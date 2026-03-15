9926a108c87402f6b5df9992b5a4648021359736
Bored Piper <boredpiper@gmail.com>
2026-03-15 22:44:32 +0530

    Fix analytics.jsx import path to use relative path

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { generateKPIReport, analyzeTrends } from '@/lib/kpiFeatures';
+import { generateKPIReport, analyzeTrends } from '../lib/kpiFeatures';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
 
